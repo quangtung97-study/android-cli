4fcef51f3950858145dc4c3aa188fa5be01f0f05
quangtung97 <quangtung29121997@gmail.com>
2018-06-20 20:11:23 +0700

    Add Makefile

---
+++ Makefile
@@ -0,0 +1,11 @@
+# Connect device through USB cable
+# adb tcpip 5555
+# Disconnect the USB cable
+# Settings > About Phone > Status > IP Address
+# adb connect device_ip_address
+# adb devices
+#
+.PHONY: main
+main: 
+	gradlew assembleDebug && \
+	adb install -r ./app/build/outputs/apk/debug/app-debug.apk
--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,12 +1,11 @@
 #include <jni.h>
 #include <string>
 
-extern "C" JNIEXPORT jstring
+extern "C" {
+    jstring Java_kstn_tung_cli_MainActivity_stringFromJNI(JNIEnv *env, jobject);
+}
 
-JNICALL
-Java_kstn_tung_cli_MainActivity_stringFromJNI(
-        JNIEnv *env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
+jstring Java_kstn_tung_cli_MainActivity_stringFromJNI(JNIEnv *env, jobject) {
+    std::string hello = "Tạ Quang Tùng + Nguyễn Ngân";
     return env->NewStringUTF(hello.c_str());
 }
